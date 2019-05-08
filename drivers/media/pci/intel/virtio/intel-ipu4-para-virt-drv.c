ffa01f0644d22812d87fd0b4de4e282abfbb1397
spoluri <sarat.chandra.poluri@intel.com>
2019-05-08 15:49:44 -0700

    media: intel-ipu4: [ICI/VIRT] Use proper locks for protecting buf list.
    Tracked-On: 1409362467
    Signed-off-by: spoluri <sarat.chandra.poluri@intel.com>

--- drivers/media/pci/intel/virtio/intel-ipu4-para-virt-drv.c
+++ drivers/media/pci/intel/virtio/intel-ipu4-para-virt-drv.c
@@ -878,7 +878,6 @@ static void base_device_release(struct device *sd)
 int virt_frame_buf_init(struct ici_isys_frame_buf_list *buf_list)
 {
 	buf_list->drv_priv = NULL;
-	mutex_init(&buf_list->mutex);
 	spin_lock_init(&buf_list->lock);
 	spin_lock_init(&buf_list->short_packet_queue_lock);
 	INIT_LIST_HEAD(&buf_list->getbuf_list);
